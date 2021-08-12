150d746d1c5be58992a388a549588fbb6f95b8d4
MS_Hasan <sumonhasan.md111@gmail.com>
2021-08-12 20:42:12 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Home Page')
+})
+
+app.get('*', (req, res) => {
+    res.send("<h1>404 Not Found</h1>")
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running http://localhost:${PORT}`)
+})
