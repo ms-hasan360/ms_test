570ab1c4e9b64f2233c7831fcd7d85a2b8f53124
MS_Hasan <sumonhasan.md111@gmail.com>
2021-08-13 11:33:07 +0600

    some content change

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require('express')
+const morgan = require('morgan')
 const PORT = process.env.PORT || 3000
+
 
 const app = express()
 // setup views engine
+app.use(morgan('dev'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.get('/signup', (req, res) => {
+    res.render('pages/signup', { title: 'MS | AH' })
+})
+
+app.get('/login', (req, res) => {
+    res.render('pages/login', { title: 'MS | AH' })
+})
+
+app.get('/dashboard', (req, res) => {
+    res.render('pages/dashboard', { title: 'MS | AH' })
+})
 
 app.get('/', (req, res) => {
-    res.render('pages/full_part')
+    res.render('pages/main', { title: 'MS | AH'})
 })
 
 app.get('*', (req, res) => {
